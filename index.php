508db59d0f07f70dec8019f72f734c2ed7973b1a
NicolaScapin <nicolascapin03@gmail.com>
2019-02-20 10:29:16 +0100

    Aggiunto form

--- index.php
+++ index.php
@@ -12,10 +12,23 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-    <p>ecco un paragrafo</p>
+    <form>   
+  <div class="form-group">
+    <label for="exampleInputEmail1">Nome</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il nome">
+  </div>
+   <div class="form-group">
+    <label for="exampleInputEmail1">Cognome</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il cognome">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <p>ecco un paragrafo</p>p>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
