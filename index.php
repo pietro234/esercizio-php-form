f8300370d8dec9c7122436b09e8250bd00dd3677
NicolaScapin <nicolascapin03@gmail.com>
2019-02-27 11:38:47 +0100

    sistemazione del form

--- index.php
+++ index.php
@@ -11,24 +11,27 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <form>   
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nome</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il nome">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputEmail1">Cognome</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il cognome">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    <div class="container">
+      <h1>Hello, world!</h1>
+      <form>   
+        <div class="form-group">
+          <label for="exampleInputEmail1">Nome</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il nome">
+        </div>
+         <div class="form-group">
+          <label for="exampleInputEmail1">Cognome</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="inserisci il cognome">
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Example textarea</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" aria-describedby="indirizzoHelp" rows="3"></textarea>
+          <small id="indirizzoHelp" class="form-text text-muted">Deve contenere la via, c.a.p. e provincia.</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
 
-    <p>ecco un paragrafo</p>p>
+    
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
